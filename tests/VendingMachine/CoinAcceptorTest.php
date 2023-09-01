82e7f9a936deb9e62b94926d0af1e1b2578bc599
Jacek Kobus <kobus.jacek@gmail.com>
2023-09-01 20:42:49 +0200

    added better coin acceptor test

--- tests/VendingMachine/CoinAcceptorTest.php
+++ tests/VendingMachine/CoinAcceptorTest.php
@@ -8,14 +8,25 @@ use PHPUnit\Framework\TestCase;
 
 class CoinAcceptorTest extends TestCase
 {
-    public function testIsAccepted()
+    public static function data(): array
+    {
+        return [
+            [Coin::of(1), true],
+            [Coin::of(2), true],
+            [Coin::of(5), true],
+            [Coin::of(10), true],
+            [Coin::of(20), true],
+            [Coin::of(50), true],
+            [Coin::of(100), false],
+        ];
+    }
+
+    /**
+     * @dataProvider data
+     */
+    public function testIsAccepted(Coin $coin, bool $isAccepted)
     {
         $coinAcceptor = new CoinAcceptor();
-        $coin = Coin::of(1);
-
-        $this->assertTrue($coinAcceptor->isAccepted($coin));
-
-        $coin = Coin::of(100);
-        $this->assertFalse($coinAcceptor->isAccepted($coin));
+        $this->assertSame($isAccepted, $coinAcceptor->isAccepted($coin));
     }
 }
